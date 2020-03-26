9b0cbd262c002a8c2755b962fc7ae7d7ca1fba24
durgauri <minochndk@gmail.com>
2020-03-26 17:03:17 +0700

    third commit

---
+++ Assets/Scripts/ToContinue.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ToContinue : MonoBehaviour
+{
+    private int sceneToContinue;
+    public void ContinueGame()
+    {
+        sceneToContinue = PlayerPrefs.GetInt("SavedScene");
+        if (sceneToContinue != 0)
+        {
+            SceneManager.LoadScene(sceneToContinue);
+        }
+        else
+        {
+            return;
+        }
+    }
+}
---
+++ Assets/Scripts/ToSave.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class ToSave : MonoBehaviour
+{
+    private int currentSceneIndex;
+    public void LoadMainMenu()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        PlayerPrefs.SetInt("SavedScene", currentSceneIndex);
+        SceneManager.LoadScene(6);
+    }
+}
